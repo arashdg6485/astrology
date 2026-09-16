48574634a2359152eb883c401a0a524b6ab83f98
arashdg6485 <arashdg6485@gmail.com>
2026-09-16 06:47:03 +0330

    Add birth data model for research reports

---
+++ app/src/main/java/com/ayat/irisresearch/BirthData.java
@@ -0,0 +1,12 @@
+package com.ayat.irisresearch;
+
+/** Simple transport object for birth information used by the research report. */
+public class BirthData {
+    public String name;
+    public String date;
+    public String time;
+    public String city;
+    public double latitude;
+    public double longitude;
+    public String timezone;
+}
